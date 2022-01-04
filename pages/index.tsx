40ef188dfec7d2fca241e2e3ac2c924d99b1c6a5
digthedill <popssly@gmail.com>
2022-01-03 20:59:07 -0600

    merging styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next'
+import styles from '../styles/Index.module.scss'
 
 const Home: NextPage = () => {
-  return <h2>main content</h2>
+  return <section className={styles.container}>
+  <h2>Dillon Kelley</h2>
+  <p>Full Stack Developer</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id gravida eros. Phasellus faucibus, dolor non dignissim luctus, nunc lacus maximus sem, et dignissim enim magna at eros. Suspendisse fringilla sem tortor, ac eleifend massa laoreet sed. Duis in scelerisque ipsum, vel ullamcorper est. Nullam sapien dolor, facilisis sed imperdiet sed, volutpat nec tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ex sapien, placerat ornare fringilla sed, fringilla et sem. Vivamus faucibus mauris purus, et scelerisque lorem laoreet vel. Suspendisse elit urna, blandit in ornare ac, sagittis non nulla. Etiam et velit ultrices, imperdiet sapien at, efficitur ante. Fusce cursus justo ut elit molestie volutpat. Maecenas mollis, tortor quis feugiat interdum, ligula turpis imperdiet arcu, vel fringilla lacus urna nec quam. In varius consectetur dolor et tincidunt. Donec volutpat sapien felis, at ornare enim facilisis at. Duis et magna eget augue ultrices euismod. Curabitur vel risus iaculis, consequat dolor at, finibus purus.</p>
+  </section>
+
 }
 
 export default Home
